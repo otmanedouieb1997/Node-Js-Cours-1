0bf379549f36d22d7b664a5fb4c2789a9cc06cb7
otmanedouieb1997 <otmane.douieb.1997@hotmail.com>
2021-10-12 23:41:09 +0100

    node js respons and request

--- app.js
+++ app.js
@@ -4,5 +4,26 @@ const init = require('./init')
 
 // Create server
 http.createServer((req, res) => {
+    //Header params
+    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('auther', init.params.user)
+    if(req.url === '/') {
+        res.end("Hello")
+    }
+    // 404 Page Not Found
+    else{
+        res.write('<html>')
+        res.write('<head>')
+        res.write('</head>')
+        res.write('<body>')
+        res.write('<div>')
+        res.write('<h1>')
+        res.write('Page Not Found!')
+        res.write('</h1>')
+        res.write('</div>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end()
+    }
     console.log('Server Starting on port 3000')
 }).listen(init.params.port)
