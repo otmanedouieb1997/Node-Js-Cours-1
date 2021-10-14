345de78913943c3cb9100cc471dde4160e147a71
otmanedouieb1997 <otmane.douieb.1997@hotmail.com>
2021-10-14 13:30:36 +0100

    start with express ans middleware

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const http = require('http')
-const fs = require('fs')
-const init = require('./init')
-const routes = require('./routes')
+const express = require('express')
 
 
 
-// Create server
-http.createServer(routes).listen(init.params.port)
+const app = express()
+
+
+app.use((req, res, next) =>{
+    console.log('=> start middleware 1' )
+    console.log(req.url)
+    console.log('=> end middleware 1' )
+    next() // move to next middleware 
+})
+
+app.use((req, res, next) =>{
+    console.log('=> start middleware 2' )
+    res.send('<h1>Welcome To Express</h1>')
+    console.log('=> end middleware 2' )
+})
+
+
+// start express server
+const server = app.listen(3000, () => {
+    console.log('===================================')
+    console.log('Express server started on port 3000')
+    console.log('===================================')
+})
